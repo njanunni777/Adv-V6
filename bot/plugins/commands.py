0081422e9b955e18a0a7acb2a39eab1ba8a49748
njanunni777 <85037210+njanunni777@users.noreply.github.com>
2021-08-20 12:46:32 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Channel', url="https://t.me/newwbox"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Channel', url='https://t.me/newwbox'),
+        InlineKeyboardButton('Group', url ='https://t.me/relaxxmovies')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Owner 🛠', url='https://t.me/G9037944937')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
