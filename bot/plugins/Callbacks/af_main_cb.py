63386856e4875a36c1ab7fd191d0c2bcdccf1c72
njanunni777 <85037210+njanunni777@users.noreply.github.com>
2021-08-20 13:04:49 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -139,7 +139,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code>"
+    text=f"<i>JOIN GROUP ☣️ @relaxxmovies ☣️Found</i> <code>{leng}</code> <i>Results For Your Request🙂:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
