d747875601ba6d872180526e94522ee77c2ec53c
njanunni777 <85037210+njanunni777@users.noreply.github.com>
2021-08-20 13:28:18 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = f"{file_name} /n @relaxxmovies",
+                caption = f"{file_name} \n @relaxxmovies",
                 parse_mode="html"
                 reply_markup=InlineKeyboardMarkup(
                     [
